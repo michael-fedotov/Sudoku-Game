bd2d83be53cccffa5a4755e23f56eefff9fb1ce8
Michael <m_fedotov@hotmail.com>
2020-10-08 01:40:46 -0400

    Made square objects which are drawn onto the board

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -1,6 +1,14 @@
 import random
-import pygame
+import pygame as pg
 from pygame import mixer
+
+
+pg.init()
+
+# Creating the screen (x, y)
+screen = pg.display.set_mode((700, 600))
+# Loading the image into the my_image variable
+my_image = pg.image.load('blank-sudoku-grid.png')
 
 
 # Sample grid
@@ -219,37 +227,125 @@ def solution(board):
     print_board(board)
 
 
+class Square:
+    """Class that will represent a sudoku square and can be filled with a number"""
+    def __init__(self, x, y, w, h, width, num=""):
+        self.rect = pg.Rect(x, y, w, h)
+        # When inactive the color will be border of the square
+        self.color_inactive = (137, 137, 137)   # (137, 137, 137)
+        # When the box is clicked it will change color until enter is pressed or another square is pressed
+        self.color_active = (20, 20, 200)
+        self.number = num
+        self.font = pg.font.Font(None, 32)
+        self.txt_surface = self.font.render(num, True, self.color_inactive)
+        # Square will only be active if it is clicked upon
+        self.active = False
+        # Width when drawing the lines
+        self.width = width
+
+    def draw(self, screen):
+        """Draws the number in the text box and the box itself"""
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pg.draw.rect(screen, self.color_inactive, self.rect, self.width)
+
+
+def create_squares():
+    """Creates a number of squares that can be drawn"""
+    # Empty list of squares
+    squares = []
+
+    # Intial y value
+    y = 4-55
+
+    # Loops through 9 times for each column and 9 times for each row. Each times adding a square object to the list of squares.
+    for i in range(9):
+        y += 55
+        x = 4-55
+        for j in range(9):
+            x += 55
+            squares.append(Square(x, y, 55, 55, 1))
+
+    return squares
+
+
+def create_borders():
+    """Function that returns a list of rectangles in form of lines so they can be drawn onto the borders of 
+    the sudoku board"""
+
+    # Initial x and y positions
+    x1 = 4
+    y1 = 4
+
+    # List to store the borderline square objects
+    squaresx = []
+    squaresy = []
+
+    # Loops 4 times as there are 4 major borders along the columns, each time adding a borderline to the list
+    for i in range(4):
+        square_to_add = Square(x1, 1, 1, 502, 4)
+        # Changing the color of the object to 0, 0, 0
+        square_to_add.color_inactive = (0, 0, 0)
+        squaresx.append(square_to_add)
+        x1 += 3 * 55
+
+    # Loops through 4 times, there are 4 major row borders, each iteration adds a borderline to the list
+    for i in range(4):
+        square_to_add = Square(1, y1, 502, 1, 4)
+        # Changing the color of the object to 0, 0, 0
+        square_to_add.color_inactive = (0, 0, 0)
+        squaresy.append(square_to_add)
+        y1 += 3 * 55
+
+    return squaresx, squaresy
+
+
 def main():
     """Calls the necessary functions to generate the puzzle, then remove the squares according to difficulty."""
+
     # fill_9(grid)
     # # Creating the root node to track of all the branching factors
     # solution(grid)
     # # difficulty_score(list_nodes)
     # print(final_puzzle())
-    
-    pygame.init()
+    # Initiliaze pg
 
-# Creating the screen (x, y)
-    screen = pygame.display.set_mode((800, 600))
-    # Loading the image into the my_image variable
-    my_image = pygame.image.load('blank-sudoku-grid.png')
+    # Creating the squares objects in the board and the border lines to block the grey lines
+    squares = create_squares()
+    squaresx, squaresy = create_borders()
+
     running = True
     while running:
-        screen.fill((0, 0, 0))
-        screen.blit(my_image, (0,0))
-        for event in pygame.event.get():
-            if event == pygame.QUIT:
+        screen.fill((255, 255, 255))
+        screen.blit(my_image, (0, 0))
+
+        # Drawing the squares on the board
+        for square in squares:
+            square.draw(screen)
+
+        # Drawing the lines on the boarders of the puzzle
+        for square in squaresx:
+            square.draw(screen)
+        for square in squaresy:
+            square.draw(screen)
+
+        # Checks each of the events
+        for event in pg.event.get():
+            # If the event is the quit event then it will exit the program
+            if event.type == pg.QUIT:
                 running = False
+
+        # Updates the display
+        pg.display.update()
 
 
 if __name__ == '__main__':
     main()
 
 
-# Starting pygame coding
+# Starting pg coding
 
-# Initiallzing pygame
-pygame.init()
+# # Initiallzing pg
+# pg.init()
 
-# Creating the screen (x, y)
-screen = pygame.display.set_mode((800, 600))
+# # Creating the screen (x, y)
+# screen = pg.display.set_mode((800, 600))
